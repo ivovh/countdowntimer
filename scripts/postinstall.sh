abdc4e120a38d746688eb962e7657f7b25d8eb64
Jan Grape <jan.grape@crisp.se>
2016-10-16 23:18:20 +0200

    Temporarily adding DNS challenge for Let's Encrypt SSL

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf public/* && mkdir -p public && cp -r src/* public && webpack
+rm -rf public/* && mkdir -p public && cp -r src/* public && cp -r src/.well* public && webpack
