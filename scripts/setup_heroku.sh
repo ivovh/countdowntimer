2f63988d00c6790002602970488c36c8057a52ca
Jan Grape <jan.grape@crisp.se>
2017-03-05 01:47:18 +0100

    Removed instructions for certificate handling, since heroku does it automatically now

--- scripts/setup_heroku.sh
+++ scripts/setup_heroku.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 heroku config:set NODE_ENV=production
 heroku config:set NPM_CONFIG_PRODUCTION=true
-
-# create local cert with
-sudo certbot certonly --manual
-
-# upload to Heroku using
-sudo heroku _certs:add /etc/letsencrypt/live/www.countdowntimer.se/fullchain.pem /etc/letsencrypt/live/www.countdowntimer.se/privkey.pem
