f1f0dd1b16cce0d136f147b803ee9251b9d0e1ef
Jan Grape <jan.grape@crisp.se>
2017-07-14 18:24:56 +0200

    Reformatted

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  entry: "./src/js/client.js",
-  output: {
-    path: __dirname + "/public/js",
-    filename: "client.js"
-  },
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: "babel-loader"
-    }]
-  },
-  resolve: {
-    alias: {
-      jquery: "jquery/src/jquery"
+    entry: "./src/js/client.js",
+    output: {
+        path: __dirname + "/public/js",
+        filename: "client.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    resolve: {
+        alias: {
+            jquery: "jquery/src/jquery"
+        }
     }
-  }
 };
